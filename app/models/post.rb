537d2a9e05e7284d7ec675e026acbf901892a1b7
Max Mezalon <jmezalon@gmail.com>
2021-12-14 09:37:02 -0500

    commit tree e1bf4a8c6cddc0e67a70e1aad77a9ba2bc648082

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    validate :clickbait
+
+    def clickbait 
+        container = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+        if container.none? { |pat| pat.match title }
+            errors.add(:title, "must be clickbait")
+        end 
+    end
 end
